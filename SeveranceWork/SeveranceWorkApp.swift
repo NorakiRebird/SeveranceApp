988c14190dd7af88392aea2d7be1531e497f182e
NorakiRebird <Rayann_Chaghla@outlook.fr>
2025-02-17 17:05:25 +0100

    Initial Commit

---
+++ SeveranceWork/SeveranceWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeveranceWorkApp.swift
+//  SeveranceWork
+//
+//  Created by Rayann chaghla on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SeveranceWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
